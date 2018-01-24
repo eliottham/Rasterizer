fd0fad35f4efa1d52662bb3be2fbbfbbc7d57787
Eliott Ham <eliotthham@gmail.com>
2018-01-23 19:59:24 -0800

    Initial Commit

---
+++ CSE_167/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE_167
+//
+//  Created by Eliott Ham on 1/23/18.
+//  Copyright © 2018 Eliott Ham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
